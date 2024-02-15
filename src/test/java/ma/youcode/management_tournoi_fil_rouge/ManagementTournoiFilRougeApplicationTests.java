c0c99e98a96ac149322bb178b965e55734b01b8a
moussafia <moussafia.mohammed1999@gmail.com>
2024-02-15 13:44:05 +0100

    first commit

---
+++ src/test/java/ma/youcode/management_tournoi_fil_rouge/ManagementTournoiFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.management_tournoi_fil_rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementTournoiFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
