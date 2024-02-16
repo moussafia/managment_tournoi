cbb7342c669d4f062e470ab4306c664e20b922f5
moussafia <moussafia.mohammed1999@gmail.com>
2024-02-16 20:40:38 +0100

    add properties.yml for spring data

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/entity/AppRole.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/entity/AppRole.java
@@ -1,12 +1,10 @@
 package ma.youcode.managment_tournoi_backend.entity;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.*;
-
 import java.util.List;
 
 @Entity
