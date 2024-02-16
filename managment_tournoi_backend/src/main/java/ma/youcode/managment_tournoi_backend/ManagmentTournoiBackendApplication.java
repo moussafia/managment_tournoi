94f395bff55cad719501a083b0859734cc816add
moussafia <moussafia.mohammed1999@gmail.com>
2024-02-16 14:49:05 +0100

    add project spring boot

---
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/ManagmentTournoiBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.managment_tournoi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentTournoiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentTournoiBackendApplication.class, args);
+	}
+
+}
---
+++ managment_tournoi_backend/src/test/java/ma/youcode/managment_tournoi_backend/ManagmentTournoiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.managment_tournoi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentTournoiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
