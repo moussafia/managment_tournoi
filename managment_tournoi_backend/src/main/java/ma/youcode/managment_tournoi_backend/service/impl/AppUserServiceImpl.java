ad6d94317d5f371315788ce9b683f93e20ce5acc
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-06 23:34:42 +0000

    Update: Modified AppUserServiceImpl.java

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/AppUserServiceImpl.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/AppUserServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import ma.youcode.managment_tournoi_backend.entity.AppUser;
 import ma.youcode.managment_tournoi_backend.repository.AppUserRepository;
 import ma.youcode.managment_tournoi_backend.service.AppUserService;
+import ma.youcode.managment_tournoi_backend.util.xlsx.XLSXFieldValidation;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +15,8 @@ public class AppUserServiceImpl implements AppUserService {
     private final AppUserRepository userRepository;
     @Override
     public List<AppUser> createListMembers(List<AppUser> members) {
-        System.out.println("hello");
-
+        XLSXFieldValidation.xlsxFieldValidationPattern(members);
+        XLSXFieldValidation.xlsxFieldValidationEmails(members);
         return userRepository.saveAll(members);
     }
 
