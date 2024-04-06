a381077209f8ab2ca8531d8f0376214894ec3be5
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-06 23:34:42 +0000

    Update: Modified ControllerAdvice.java

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/rest/handler/ControllerAdvice.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/rest/handler/ControllerAdvice.java
@@ -1,4 +1,19 @@
-package ma.youcode.managment_tournoi_backend.rest.advise;
+package ma.youcode.managment_tournoi_backend.rest.handler;
 
-public class COntrollerAdvice {
+import ma.youcode.managment_tournoi_backend.exception.XLSXFieldException;
+import ma.youcode.managment_tournoi_backend.util.response.XlsxFieldErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(value = XLSXFieldException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<XlsxFieldErrorResponse> handleXlsxFieldError(XLSXFieldException xlsxFieldException) {
+        final XlsxFieldErrorResponse xlsxFieldErrorResponse = new XlsxFieldErrorResponse(xlsxFieldException.getMessage(), xlsxFieldException.getValidationFieldMaps());
+        return new ResponseEntity<>(xlsxFieldErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
