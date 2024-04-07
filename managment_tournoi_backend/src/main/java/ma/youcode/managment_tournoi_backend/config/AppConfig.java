7ae04c5f784de16447fefde126f9d48d1bed7ec3
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-07 11:38:54 +0000

    Update AppConfig.java based on conversation

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/config/AppConfig.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/config/AppConfig.java
@@ -3,13 +3,22 @@ package ma.youcode.managment_tournoi_backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 
 @Configuration
 public class AppConfig {
     @Bean
     public ResourceLoader resourceLoader(){
         return new DefaultResourceLoader();
+    }
+    @Bean
+    @Primary
+    public FreeMarkerConfigurationFactory freeMarkerConfigurationFactory(){
+        FreeMarkerConfigurationFactory freeMarkerConfigurationFactory = new FreeMarkerConfigurationFactory();
+        freeMarkerConfigurationFactory.setTemplateLoaderPath("classpath:/templates");
+        return freeMarkerConfigurationFactory;
     }
 }
