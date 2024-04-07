41b6c2e042e2db635a9326bdf7a864741e73b34a
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-07 11:38:54 +0000

    Update MailRequest.java with new properties for email sending

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/mail/MailRequest.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/mail/MailRequest.java
@@ -1,4 +1,12 @@
 package ma.youcode.managment_tournoi_backend.util.mail;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class MailRequest {
+    public String name;
+    public String to;
+    public String subject;
 }
