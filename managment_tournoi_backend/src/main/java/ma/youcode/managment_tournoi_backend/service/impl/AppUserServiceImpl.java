db813ed866c78b92bc9a2c817ee75bc1204e80ea
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-07 11:38:54 +0000

    Implement createListMembers method in AppUserServiceImpl.java

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/AppUserServiceImpl.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/AppUserServiceImpl.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import ma.youcode.managment_tournoi_backend.entity.AppUser;
 import ma.youcode.managment_tournoi_backend.repository.AppUserRepository;
 import ma.youcode.managment_tournoi_backend.service.AppUserService;
+import ma.youcode.managment_tournoi_backend.util.mail.MailRequest;
+import ma.youcode.managment_tournoi_backend.util.mail.MailUtil;
+import ma.youcode.managment_tournoi_backend.util.member.MemberUtils;
 import ma.youcode.managment_tournoi_backend.util.xlsx.XLSXFieldValidation;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -17,6 +22,15 @@ public class AppUserServiceImpl implements AppUserService {
     public List<AppUser> createListMembers(List<AppUser> members) {
         XLSXFieldValidation.xlsxFieldValidationPattern(members);
         XLSXFieldValidation.xlsxFieldValidationEmails(members);
+        for (AppUser member : members) {
+            String password = MemberUtils.generatePassword();
+            String username = MemberUtils.generateUsername(member.getFirstName(), member.getLastName());
+            member.setUsername(username);
+            member.setPassword(MemberUtils.encodePassword(password));
+            member.setCreatedAt(LocalDateTime.now());
+            member.setIsDeleted(Boolean.FALSE);
+            MailUtil.sendMail(member, password);
+        }
         return userRepository.saveAll(members);
     }
 
