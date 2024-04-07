cc51a8f0381896689a3bf09f7145986ffa59eb96
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-07 11:38:54 +0000

    Update MailResponse.java according to requirements

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/mail/MailResponse.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/mail/MailResponse.java
@@ -1,4 +1,15 @@
 package ma.youcode.managment_tournoi_backend.util.mail;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MailResponse {
+    private String message;
+    private Boolean status;
 }
