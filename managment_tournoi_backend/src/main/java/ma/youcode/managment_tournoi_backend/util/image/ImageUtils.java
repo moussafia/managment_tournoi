6504bdca74d25a830cec4a0f07499495ec2fc669
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-08 11:31:23 +0000

    in pom i add dependency cloudinary and i add images upload and get

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/image/ImageUtils.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/util/image/ImageUtils.java
@@ -1,4 +1,18 @@
 package ma.youcode.managment_tournoi_backend.util.image;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public class ImageUtils {
+    public static String saveImageToStorage(MultipartFile image, String path) throws IOException {
+        String pathToSave = "";
+
+    }
+    public static byte[] getImages(String imageDirectory, String imageName) throws IOException {
+
+    }
+    public static boolean deleteImage(String imageDirectory, String imageName) throws IOException {
+
+    }
 }
