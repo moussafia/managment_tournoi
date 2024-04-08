e97f2819a29ddc0d763ee2e01908cb819997b4ad
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-08 11:31:23 +0000

    in pom i add dependency cloudinary and i add images upload and get

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/TeamService.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/TeamService.java
@@ -1,4 +1,12 @@
 package ma.youcode.managment_tournoi_backend.service;
 
+import ma.youcode.managment_tournoi_backend.entity.Team;
+
+import java.util.List;
+
 public interface TeamService {
+    List<Team> getAllTeams();
+    Team getTeamById(Long id);
+    Team createTeam(Team team);
+    Team updateTeam(Team team);
 }
