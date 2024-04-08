b4ea55be4625f765b7318201592052715b9725b1
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-08 11:31:23 +0000

    in pom i add dependency cloudinary and i add images upload and get

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/repository/TeamRepository.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/repository/TeamRepository.java
@@ -1,4 +1,9 @@
 package ma.youcode.managment_tournoi_backend.repository;
 
-public interface TeamRepository {
+import ma.youcode.managment_tournoi_backend.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
