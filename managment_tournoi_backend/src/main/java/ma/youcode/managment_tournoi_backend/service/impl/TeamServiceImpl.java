53ca171850813df04366d4151dcb2f2339266014
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-08 11:31:23 +0000

    in pom i add dependency cloudinary and i add images upload and get

--- managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/TeamServiceImpl.java
+++ managment_tournoi_backend/src/main/java/ma/youcode/managment_tournoi_backend/service/impl/TeamServiceImpl.java
@@ -1,19 +1,26 @@
 package ma.youcode.managment_tournoi_backend.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import ma.youcode.managment_tournoi_backend.entity.Team;
+import ma.youcode.managment_tournoi_backend.exception.EntityNotFoundException;
+import ma.youcode.managment_tournoi_backend.repository.TeamRepository;
 import ma.youcode.managment_tournoi_backend.service.TeamService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
 public class TeamServiceImpl implements TeamService {
+    private final TeamRepository teamRepository;
     @Override
     public List<Team> getAllTeams() {
-        return List.of();
+        return teamRepository.findAll();
     }
 
     @Override
-    public Team getTeamById(int id) {
-        return null;
+    public Team getTeamById(Long id) {
+
+        return teamRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Team with ID " + id + " not found"));
     }
 
     @Override
