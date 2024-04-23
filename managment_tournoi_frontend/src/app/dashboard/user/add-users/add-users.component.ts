ad0ccb130a347de8dd846caa829c58ae626b443d
moussafia.mohammed1999@gmail.com <moussafia.mohammed1999@gmail.com>
2024-04-23 12:20:32 +0100

    fixing probelem sweet alert and display message error

--- managment_tournoi_frontend/src/app/dashboard/user/add-users/add-users.component.ts
+++ managment_tournoi_frontend/src/app/dashboard/user/add-users/add-users.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { catchError, map, of, startWith } from 'rxjs';
 import { MemberSaveResponseDto } from 'src/app/dto/appUserFileDto/createMemberDto/MemberSaveResponseDto';
 import { ValidationFieldMaps } from 'src/app/dto/appUserFileDto/createMemberDto/validationFieldMaps';
 import { DataResponse, DataState } from 'src/app/dto/data.state.';
 import { UserService } from 'src/app/services/user/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-users',
@@ -19,7 +21,7 @@ memberFileSaveResponse?: DataResponse<MemberSaveResponseDto, ValidationFieldMaps
 
 readonly dataState = DataState;
 
-@ViewChild('succesInsertSwal') succesInsertSwal?: SwalComponent;
+@ViewChild('formAddUsersXlsx') formAddUsersXlsx?: NgForm;
 
 constructor(private userService: UserService){}
 
@@ -44,10 +46,42 @@ addUsers() {
     next: data =>{
         this.memberFileSaveResponse = data as DataResponse<MemberSaveResponseDto, any>
         if (data.dataState === this.dataState.LOADED ) {
-          this.succesInsertSwal?.fire();
-          console.log("1" + this.memberFileSaveResponse.data?.message)
-          console.log(this.memberFileSaveResponse?.dataState)
-        }}
+            this.swalSucces(this.memberFileSaveResponse?.data?.message);
+            this.formAddUsersXlsx?.reset();
+            const fileInput = document.querySelector('input[type="file"]');
+        } else if( data.dataState === this.dataState.ERROR ){
+          let messageError = this.memberFileSaveResponse?.error?.['message'];
+          let fields  = this.memberFileSaveResponse?.error?.['field'];
+          let textError = `<h1 class='text-lg text-red-700 text-center my-4'>${messageError}</h1>`;
+          for(const key in fields){           
+                let fieldsArray = fields[parseInt(key)];
+                textError += `<div class="p-2 mb-2 rounded">`;
+                textError += `<div class="font-bold mb-1 text-md">row number ${key} in excel :</div>`;
+                textError += `<ul class="list-disc flex flex-col justify-start items-start">`;
+                for(let g = 0; g < fieldsArray.length; g++){
+                  textError += `<li class='py-2'> - ${fieldsArray[g]}</li>`;
+                }
+                textError += `</ul>`;
+                textError += `</div>`;
+          }
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                html: textError,
+              });
+        }
+      }
+        
   })
+  }
+
+  swalSucces(message: string | undefined){
+      Swal.fire({
+        position : "center",
+        icon :"success",
+        title : message,
+        showConfirmButton : false,
+        timer : 3500 
+      })
   }
 }
